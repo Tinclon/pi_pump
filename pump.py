6a41ce6a6ab4f352773661a507ca8df8d8bee47f
Tinclon <clinton.h.nielsen@gmail.com>
2020-03-11 07:54:17 -0600

    Update pump.py

--- pump.py
+++ pump.py
@@ -3,11 +3,16 @@
 from time import sleep
 from RPi.GPIO as GPIO
 
-POWER_PIN  = 18
-GROUND_PIN = 20
-INPUT_PIN  = 22
+SENSOR_POWER_ON   = GPIO.HIGH
+SENSOR_POWER_PIN  = 18
+SENSOR_GROUND_PIN = 20
+SENSOR_INPUT_PIN  = 22
 
-OUTPUT_PIN = 8
+PUMP_POWER_PIN    = 4
+PUMP_GROUND_PIN   = 6
+PUMP_CONTROL_PIN  = 8
+PUMP_CONTROL_ON   = GPIO.LOW
+PUMP_CONTROL_OFF  = GPIO.HIGH
 
 def cleanup():
     GPIO.setwarnings(False)
@@ -15,41 +20,41 @@ def cleanup():
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(POWER_PIN, GPIO.OUT)
-    GPIO.setup(OUTPUT_PIN, GPIO.OUT)
-    GPIO.output(POWER_PIN, GPIO.HIGH)
-    GPIO.output(OUTPUT_PIN, GPIO.HIGH)
+    GPIO.setup(SENSOR_INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(SENSOR_POWER_PIN, GPIO.OUT)
+    GPIO.setup(PUMP_CONTROL_PIN, GPIO.OUT)
+    GPIO.output(SENSOR_POWER_PIN, SENSOR_POWER_ON)
+    GPIO.output(PUMP_CONTROL_PIN, PUMP_CONTROL_OFF)
 
 
 def inputChange():
-    GPIO.remove_event_detect(INPUT_PIN)
+    GPIO.remove_event_detect(SENSOR_INPUT_PIN)
     pumpIsOn = False
     currentSpeed = 1
     try:
         while currentSpeed > 0 and currentSpeed < 100:
             hits = 0
             for x in range (0,100):
-                hits += GPIO.input(INPUT_PIN)
+                hits += GPIO.input(SENSOR_INPUT_PIN)
                 sleep(0.001)
             currentSpeed = hits
             if pumpIsOn is False and currentSpeed > 1:
                 print("PUMP ON")
                 pumpIsOn = True
-                GPIO.output(OUTPUT_PIN, GPIO.LOW)
+                GPIO.output(PUMP_CONTROL_PIN, PUMP_CONTROL_ON)
             #if pumpIsOn = True:
             #    print("Current Speed: ", hits)
     finally:
         if pumpIsOn is True:
             print("PUMP OFF")
-        GPIO.output(OUTPUT_PIN, GPIO.HIGH)
+        GPIO.output(PUMP_CONTROL_PIN, PUMP_CONTROL_OFF)
         listen()
 
 def listen():
     cleanup()
     setup()
     print("LISTENING")
-    GPIO.add_event_detect(INPUT_PIN, GPIO.BOTH, callback=lambda pin: inputChange(), bouncetime = 100)
+    GPIO.add_event_detect(SENSOR_INPUT_PIN, GPIO.BOTH, callback=lambda pin: inputChange(), bouncetime = 100)
     try:
         while True:
             sleep(1)
