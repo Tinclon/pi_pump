a521904ce83b8125c6f283f5e1512e7e1320882a
Tinclon <clinton.h.nielsen@gmail.com>
2020-12-05 12:52:21 -0700

    Update pi_pump_monitor.sh

--- pi_pump_monitor.sh
+++ pi_pump_monitor.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-# pi_pump monitor
-# Remember to make me executable: sudo chmod a+x pi_pump_monitor.sh
+# pump monitor
+# Remember to make me executable: sudo chmod a+x pump_monitor.sh
 
 clear
-echo "Starting pi_pump monitor"
+echo "Starting pump monitor"
 
-pi_pump_is_running=false
+pump_is_running=false
 
 while true
 do
-  ps aux | grep '[p]ump.py' >/dev/null && pi_pump_is_running=true
-  if [ "$pi_pump_is_running" = true ]
+  ps aux | grep '[p]ump.py' >/dev/null && pump_is_running=true
+  if [ "$pump_is_running" = true ]
   then
-    #echo "pi_pump is running"
-    pi_pump_is_running=true
+    #echo "pump is running"
+    pump_is_running=true
   else
-    echo "pi_pump is not running - starting it up"
-    pi_pump_is_running=false
+    echo "pump is not running - starting it up"
+    pump_is_running=false
     nohup sudo python pump.py &>/dev/null &
   fi
   sleep 5
-  pi_pump_is_running=false
+  pump_is_running=false
 done
